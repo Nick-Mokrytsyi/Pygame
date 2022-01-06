2c4c664bae5a5ac6e66658f497eb343da531f5e4
Nick_Mokrytskyi <mykola.mokrytskyi@gmail.com>
2022-01-06 15:04:40 +0100

    Feat: Created a new file that is responsible for statistics in the game.

---
+++ stats.py
@@ -0,0 +1,9 @@
+class Stats():
+
+    def __init__(self):
+        """" """
+        self.reset_stats()
+
+    def reset_stats(self):
+        """"The stats changing during the game"""
+        self.guns_left = 2
