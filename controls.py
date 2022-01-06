f084803fe0bba80546403de96a33468d8f08756b
Nick_Mokrytskyi <mykola.mokrytskyi@gmail.com>
2022-01-06 13:46:43 +0100

    feat: Create a new class enemy. Give them the option to move on the gun and make a pattern on the coordinates x and y

--- controls.py
+++ controls.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 
 from bullet import Bullet
+from enemy import Enemy
 
 
 def events(screen, gun, bullets):
@@ -29,14 +30,15 @@ def events(screen, gun, bullets):
                 gun.move_left = False
 
 
-
-def update(screen, background_color, gun, bullets):
+def update(screen, background_color, gun, enemys,  bullets):
     """Update the display of game"""
     screen.fill(background_color)
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     gun.show()
+    enemys.draw(screen)
     pygame.display.flip()
+
 
 def update_bullets(bullets):
     """Update the coordinates of bullets and delete them from the list"""
@@ -44,3 +46,24 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
+
+def update_enemys(enemy):
+    enemy.update()
+
+def create_army(screen, enemys):
+    enemy = Enemy(screen)
+    enemy_width = enemy.rect.width
+    number_enemy_x = int((700 - 2 * enemy_width) / enemy_width)
+    enemy_height = enemy.rect.height
+    number_enemy_y = int((800 - 100 - 2 * enemy_height) / enemy_height)
+    for row_number in range(number_enemy_y - 2):
+        for enemy_number in range(number_enemy_x):
+            enemy = Enemy(screen)
+            enemy.x = enemy_width + (enemy_number * enemy_width)
+            enemy.y = enemy_height + (row_number * enemy_height)
+            enemy.rect.x = enemy.x
+            enemy.rect.y = enemy.rect.height + (enemy.rect.height * row_number)
+            enemys.add(enemy)
+
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 import controls
 from space_gun import Space_Rocket
 from pygame.sprite import Group
+from enemy import Enemy
 
 
 def start():
@@ -12,12 +13,15 @@ def start():
     background_color = (0, 0, 0)
     gun = Space_Rocket(screen)
     bullets = Group()
+    enemys = Group()
+    controls.create_army(screen, enemys)
 
     while True:
         controls.events(screen, gun, bullets)
         gun.update_gun()
-        controls.update(screen, background_color, gun, bullets)
+        controls.update(screen, background_color, gun, enemys, bullets)
         controls.update_bullets(bullets)
+        controls.update_enemys(enemys)
 
 
 start()
